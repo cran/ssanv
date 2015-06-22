7d1b81f16322a80d772409aec5854b9be1ca825a
Michael P. Fay <mfay@niaid.nih.gov>
2015-06-22 00:00:00 +0000

    version 1.1

--- R/uniroot.integer.R
+++ R/uniroot.integer.R
@@ -23,6 +23,10 @@ maxiter = 1000, ...) {
      ever.switched<-FALSE
      tried.extreme<-FALSE
      while (step.power>-1){
+        # Jun-22-2015: fix problems when f(i)=0 exactly for some i
+        # break out when f.old=0, since xold will be the root 
+        if (f.old==0) break()
+        if (iter>=maxiter) stop("reached maxiter without a solution")
         xnew<- xold + sign*2^step.power
         if ((step.up & xnew< upper) || (!step.up & xnew> lower) ){  
 		f.new<-f(xnew,...) 
@@ -44,10 +48,17 @@ maxiter = 1000, ...) {
                   xswitch<-x.extreme
                   f.switch<-f.extreme
 			if (print.steps){ print(paste("x=",x.extreme," f(x)=",f.extreme)) }
+                  # Jun-22-2015: fix problems when f(i)=0 exactly for some i
+                  # break out when f=0, since then x will be the root
+                  if (f.extreme==0){
+                      # set xold to root and f.old=f(root) 
+                      xold<-x.extreme
+                      f.old<-f.extreme
+                      break()
+                  }
                	if (f.old*f.extreme>=0){stop("f() at extremes not of opposite sign")}
 			}
 		 }
-
 
         if (f.old*f.new <0){ 
                  sign<- sign*(-1)
@@ -67,13 +78,26 @@ maxiter = 1000, ...) {
         f.old<-f.new
 
       }
-  if (pos.side){  root<-ifelse(f.new>0,xnew,xswitch) 
-              f.root<-ifelse(f.new>0,f.new,f.switch) 
-   }
-  else{ root<-ifelse(f.new<0,xnew,xswitch)
+    # Jun-22-2015: fix problems when f(i)=0 exactly for some i
+    # for breaks out of while loop for f.old==0, make sure 
+    # get the right root
+    if (f.old==0){
+       root<-xold
+       f.root<-f.old
+    } else if (f.new==0){
+       root<-xnew
+       f.root<-f.new
+    } else if (f.switch==0){
+       root<-xswitch
+       f.root<-f.switch
+    } else if (pos.side){  
+        root<-ifelse(f.new>0,xnew,xswitch) 
+        f.root<-ifelse(f.new>0,f.new,f.switch) 
+   } else { 
+        root<-ifelse(f.new<0,xnew,xswitch)
         f.root<-ifelse(f.new<0,f.new,f.switch) 
-         }  
+   }  
    # list(xold=xold,f.old=f.old,xswitch=xswitch,f.switch=f.switch,xnew=xnew,f.new=f.new,root=root)
-    list(iter=iter,f.root=f.root,root=root)
+   list(iter=iter,f.root=f.root,root=root)
 }
 
